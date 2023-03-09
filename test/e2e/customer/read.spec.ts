fbc3003ef0ff3eede896cd2ae56f005058aca876
Martien Dermawan Tanama <martiendt@gmail.com>
2023-03-09 14:42:04 +0700

    init

--- test/e2e/customer/read.spec.ts
+++ test/e2e/customer/read.spec.ts
@@ -36,33 +36,19 @@ describe("list all customers", () => {
 
     // create data
     const data = {
-      code: "A1",
-      name: "customer A",
-      chartOfAccount: "Goods",
-      hasProductionNumber: true,
-      hasExpiryDate: false,
-      unit: "pcs",
-      converter: [
-        {
-          name: "dozen",
-          multiply: 12,
-        },
-      ],
+      code: "CS1",
+      name: "John Doe",
+      email: "johndoe@example.com",
+      address: "21 Street",
+      phone: "08123456789",
     };
     await request(app).post("/v1/customers").send(data).set("Authorization", `Bearer ${accessToken}`);
     const data2 = {
-      code: "A2",
-      name: "customer B",
-      chartOfAccount: "Goods",
-      hasProductionNumber: true,
-      hasExpiryDate: false,
-      unit: "pcs",
-      converter: [
-        {
-          name: "dozen",
-          multiply: 12,
-        },
-      ],
+      code: "CS2",
+      name: "John Doe2",
+      email: "johndoe2@example.com",
+      address: "21 Street",
+      phone: "08123456789",
     };
     await request(app).post("/v1/customers").send(data2).set("Authorization", `Bearer ${accessToken}`);
 
@@ -73,22 +59,18 @@ describe("list all customers", () => {
     expect(response.body.data[0]._id).not.toBeNull();
     expect(response.body.data[0].code).toEqual(data.code);
     expect(response.body.data[0].name).toEqual(data.name);
-    expect(response.body.data[0].chartOfAccount).toEqual(data.chartOfAccount);
-    expect(response.body.data[0].hasProductionNumber).toEqual(data.hasProductionNumber);
-    expect(response.body.data[0].hasExpiryDate).toEqual(data.hasExpiryDate);
-    expect(response.body.data[0].unit).toEqual(data.unit);
-    expect(response.body.data[0].converter).toEqual(data.converter);
+    expect(response.body.data[0].email).toEqual(data.email);
+    expect(response.body.data[0].address).toEqual(data.address);
+    expect(response.body.data[0].phone).toEqual(data.phone);
     expect(response.body.data[0].createdAt instanceof Date).toBeTruthy();
     expect(response.body.data[0].createdBy_id).toBe(authResponse.body._id);
 
     expect(response.body.data[1]._id).not.toBeNull();
     expect(response.body.data[1].code).toEqual(data2.code);
     expect(response.body.data[1].name).toEqual(data2.name);
-    expect(response.body.data[1].chartOfAccount).toEqual(data2.chartOfAccount);
-    expect(response.body.data[1].hasProductionNumber).toEqual(data2.hasProductionNumber);
-    expect(response.body.data[1].hasExpiryDate).toEqual(data2.hasExpiryDate);
-    expect(response.body.data[1].unit).toEqual(data2.unit);
-    expect(response.body.data[1].converter).toEqual(data2.converter);
+    expect(response.body.data[1].email).toEqual(data2.email);
+    expect(response.body.data[1].address).toEqual(data2.address);
+    expect(response.body.data[1].phone).toEqual(data2.phone);
     expect(response.body.data[1].createdAt instanceof Date).toBeTruthy();
     expect(response.body.data[1].createdBy_id).toBe(authResponse.body._id);
 
@@ -105,7 +87,9 @@ describe("read customer", () => {
     // send request to create customer
     const response = await request(app).get("/v1/customers");
     expect(response.statusCode).toEqual(401);
-    expect(response.body.message).toBe("Unauthorized Access");
+    expect(response.body.code).toBe(401);
+    expect(response.body.status).toBe("Unauthorized");
+    expect(response.body.message).toBe("Authentication credentials is invalid.");
   });
   it("should check user have permission to access", async () => {
     const app = await createApp();
@@ -119,7 +103,9 @@ describe("read customer", () => {
     const response = await request(app).get("/v1/customers").set("Authorization", `Bearer ${accessToken}`);
 
     expect(response.statusCode).toEqual(403);
-    expect(response.body.message).toBe("Forbidden Access");
+    expect(response.body.code).toBe(403);
+    expect(response.body.status).toBe("Forbidden");
+    expect(response.body.message).toBe("Don't have necessary permissions for this resource.");
   });
   it("should read data from database", async () => {
     const app = await createApp();
@@ -151,11 +137,9 @@ describe("read customer", () => {
     expect(response.body.data._id).not.toBeNull();
     expect(response.body.data.code).toEqual(data.code);
     expect(response.body.data.name).toEqual(data.name);
-    expect(response.body.data.chartOfAccount).toEqual(data.chartOfAccount);
-    expect(response.body.data.hasProductionNumber).toEqual(data.hasProductionNumber);
-    expect(response.body.data.hasExpiryDate).toEqual(data.hasExpiryDate);
-    expect(response.body.data.unit).toEqual(data.unit);
-    expect(response.body.data.converter).toEqual(data.converter);
+    expect(response.body.data.email).toEqual(data.email);
+    expect(response.body.data.address).toEqual(data.address);
+    expect(response.body.data.phone).toEqual(data.phone);
     expect(response.body.data.createdAt instanceof Date).toBeTruthy();
     expect(response.body.data.createdBy_id).toBe(authResponse.body._id);
   });
