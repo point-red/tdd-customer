6d0218289697f3153699a1a5306f4a7016cb8d97
mohin <mohin@formics.io>
2023-03-17 16:57:48 +0530

    Added middleware

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,25 +1,56 @@
-import { Request, Response, NextFunction } from "express";
+import { RoleInterface } from "@src/modules/roles/entities/role.entity.js";
+import { db } from "@src/database/database.js";
+import { ReadRoleService } from "@src/modules/roles/services/read.service.js";
+import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
+import { ReadUserService } from "@src/modules/users/services/read.service.js";
 
-interface JwtPayload {
-  _id: string;
-}
-
-export const userMiddleware = (req: Request, res: Response, next: NextFunction) => {
+export async function userMiddleware(req: Request, res: Response, next: NextFunction) {
   const authHeader = req.headers.authorization;
 
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    return res.status(401).json({ message: "Unauthorized Access" });
+  if (!authHeader) {
+    return res.status(403).json({
+      code: 403,
+      status: "Forbidden",
+      message: "Unauthorized Access",
+    });
   }
 
-  const token = authHeader.split(" ")[1];
+  const accessToken = authHeader.split(" ")[1];
+
   try {
-    const decodedToken = jwt.verify(token, "process.env.JWT_SECRET") as JwtPayload;
-    const requser = decodedToken._id;
-    console.log("DATA:", requser);
+    const decodedToken: any = jwt.verify(accessToken, "process.env.JWT_SECRET");
+    const readUserService = new ReadUserService(db);
+    const user = await readUserService.handle(decodedToken._id, {
+      restrictedFields: ["password"],
+    });
+
+    if (!user) {
+      return res.status(403).json({
+        code: 403,
+        status: "Forbidden",
+        message: "Authentication credentials are invalid.",
+      });
+    }
+
+    const readRoleService = new ReadRoleService(db);
+    const result = (await readRoleService.handle(user.role_id)) as RoleInterface;
+
+    if (!result.permissions.includes("read-user")) {
+      return res.status(403).json({
+        code: 403,
+        status: "Forbidden",
+        message: "Don't have necessary permissions for this resource.",
+      });
+    }
 
     next();
   } catch (error) {
-    return res.status(401).json({ message: "Unauthorized Access" });
+    return res.status(403).json({
+      code: 403,
+      status: "Forbidden",
+      message: "Don't have necessary permissions for this resource.",
+    });
   }
-};
+}
+
